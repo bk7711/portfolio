32339015e9e9d85ccbb13ae145c8a9224edd0e97
Carolyn B <bmathematics@gmail.com>
2022-07-05 16:11:41 -0400

    correct logic added for creation of cards based on language and projects filtered

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 var modalBoxEl = document.getElementsByClassName("modal-trigger");
+let sampleModalEl = document.querySelector(".sampleModal");
 let targetCategory = "";
 let projects = [];
 let portfolio = [
   {
     title: "Digital Notebook",
     category: "HTML/CSS/JS",
-    image: "../images/digitalNotebook.png",
+    image: "assets/images/digitalNotebook.png",
     website: "https://pure-basin-17915.herokuapp.com/",
     github: "https://github.com/bk7711/digital-notebook",
   },
@@ -20,92 +21,121 @@ let portfolio = [
   {
     title: "Weather Dashboard",
     category: "HTML/CSS/JS",
-    image: "../images/weather.png",
+    image: "assets/images/weather.png",
     website: "https://bk7711.github.io/weatherSearch",
     github: "https://github.com/bk7711/weatherSearch",
   },
   {
     title: "Horiseon",
     category: "HTML/CSS/JS",
-    image: "../images/horiseon.png",
+    image: "assets/images/horiseon.png",
     website: "https://bk7711.github.io/horiseon/",
     github: "https://github.com/bk7711/horiseon",
   },
   {
     title: "Oui'd Cavern",
     category: "React/MERN",
-    image: "../images/ouid_cavern.png",
+    image: "assets/images/ouid_cavern.png",
     website: "https://fathomless-peak-27221.herokuapp.com/",
     github: "https://github.com/ChristanFox/silver-giggle",
   },
   {
     title: "Day Planner",
     category: "HTML/CSS/JS",
-    image: "../images/dayPlanner.png",
+    image: "assets/images/dayPlanner.png",
     website: "https://bk7711.github.io/dayPlanner/",
     github: "https://github.com/bk7711/dayPlanner",
   },
   {
     title: "Photo Port",
     category: "React/MERN",
-    image: "../images/photoPort.png",
+    image: "assets/images/photoPort.png",
     website: "#",
     github: "https://github.com/bk7711/photo-port",
   },
   {
     title: "Random Password",
     category: "HTML/CSS/JS",
-    image: "../images/password.png",
+    image: "assets/images/password.png",
     website: "https://bk7711.github.io/randomPassword",
     github: "https://github.com/bk7711/randomPassword",
   },
   {
     title: "Code Quiz",
     category: "HTML/CSS/JS",
-    image: "../images/webpage.png",
+    image: "assets/images/webpage.png",
     website: "https://bk7711.github.io/onlineQuiz/",
     github: "https://github.com/bk7711/CodeQuiz",
   },
   {
     title: "Readme Generator",
     category: "Node.js",
-    image: "../images/readmeGenerator.png",
+    image: "assets/images/readmeGenerator.png",
     website: "#",
     github: "https://github.com/bk7711/ReadmeGenerator",
   },
   {
     title: "Run Buddy",
     category: "HTML/CSS/JS",
-    image: "../images/runbuddy.png",
+    image: "assets/images/runbuddy.png",
     website: "https://bk7711.github.io/runbuddy/",
     github: "https://github.com/bk7711/runbuddy",
   },
   {
     title: "Git It Done",
     category: "HTML/CSS/JS",
-    image: "../images/gitItDone.png",
+    image: "assets/images/gitItDone.png",
     website: "https://bk7711.github.io/git-it-done/",
     github: "https://github.com/bk7711/git-it-done",
   },
   {
     title: "Pizza Hunt",
     category: "MongoDB",
-    image: "../images/pizzaHunt.png",
+    image: "assets/images/pizzaHunt.png",
     website: "#",
     github: "https://github.com/bk7711/pizza-hunt",
   },
 ];
 // fix
-let getProjects = () => {
-  let projects = portfolio.filter(function (project) {
+const getProjects = () => {
+  projects = portfolio.filter(function (project) {
     if (project.category == targetCategory) {
       return true;
     }
   });
   console.log(projects);
+  displayProjects();
 };
 
-let setCategory = () => {
+const displayProjects = () => {
+  for (let i = 0; i < projects.length; i++) {
+    let card = document.createElement("div");
+    card.setAttribute("class", "sites");
+    let cardImage = document.createElement("img");
+    cardImage.setAttribute("class", "image");
+    cardImage.setAttribute("src", projects[i].image);
+    card.appendChild(cardImage);
+    let viewSite = document.createElement("button");
+    viewSite.setAttribute("class", "btn");
+    viewSite.innerHTML = `<a href="${projects[i].website}">View Website</a>`;
+    card.appendChild(viewSite);
+
+    let cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body");
+    let title = document.createElement("h3");
+    title.setAttribute("class", "title");
+    title.textContent = projects[i].title;
+    cardBody.appendChild(title);
+    let github = document.createElement("a");
+    github.setAttribute("href", projects[i].github);
+    github.textContent = "Github Repository";
+    cardBody.appendChild(github);
+    card.appendChild(cardBody);
+
+    sampleModalEl.appendChild(card);
+  }
+};
+const setCategory = () => {
   for (var i = 0; i < modalBoxEl.length; i++) {
     modalBoxEl[i].addEventListener("click", function (e) {
       console.log(e.target);
